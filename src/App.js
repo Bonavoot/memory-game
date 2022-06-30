a7a00a79cbb5c5ff2938cd2d4222e550a60ca03e
Joey Bonventre <jbonventre95@gmail.com>
2022-06-30 18:02:30 -0400

    Added random Image API

--- src/App.js
+++ src/App.js
@@ -1,51 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Scoreboard from "./components/Scoreboard";
 import uniqid from "uniqid";
+import axios from "axios";
 
-const colorArr = [
-  { src: "orange" },
-  { src: "yellow-orange" },
-  { src: "yellow" },
-  { src: "yellow-green" },
-  { src: "green" },
-  { src: "blue-green" },
-  { src: "blue" },
-  { src: "blue-violet" },
-  { src: "violet" },
-  { src: "red-violet" },
-  { src: "red" },
-  { src: "red-orange" },
-];
-
-let checkWinner = [];
 let showOrHide = { display: "block" };
+let renderOnPlay = false;
+let imgArr = [];
+let checkWinner = [];
 
 const App = (props) => {
-  const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [giphy, setGiphy] = useState([]);
+  const [fetching, setFetching] = useState("false");
 
-  const shuffleCards = (e) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiRoot = "https://api.giphy.com/v1/gifs/";
+      const api_key = process.env.REACT_APP_GIPHY_KEY;
+      const result = await axios(`${apiRoot}trending?api_key=${api_key}`);
+      imgArr = [];
+      for (let i = 0; i < 12; i++) {
+        imgArr.push({ src: `${result.data.data[i].images.fixed_height.url}` });
+      }
+    };
+    fetchData();
+  }, [fetching]);
+
+  const shuffleImgs = (e) => {
     showOrHide = { display: "none" };
-    console.log(e.target.className);
-    const shuffledCards = [...colorArr]
+
+    const shuffledImgs = [...imgArr]
       .sort(() => Math.random() - 0.5)
       // .map gives each card object in array an id
-      .map((card) => ({ ...card, id: uniqid() }));
+      .map((url) => ({ ...url, id: uniqid() }));
+    console.log(e.target.className);
 
     if (checkWinner.includes(e.target.className)) {
       checkWinner = [];
+      imgArr = [];
+      renderOnPlay = false;
       setTurns(0);
-      setCards([]);
+      setFetching(!fetching);
       showOrHide = { display: "block" };
     } else if (e.target.className !== "material-symbols-outlined") {
       checkWinner.push(e.target.className);
-      setCards(shuffledCards);
+      setGiphy(shuffledImgs);
       setTurns(turns + 1);
-    } else {
-      setCards(shuffledCards);
+    } else if (e.target.className) {
+      setGiphy(shuffledImgs);
+      renderOnPlay = true;
     }
   };
+
+  // const shuffleCards = (e) => {
+  //   showOrHide = { display: "none" };
+
+  //   const shuffledCards = [...colorArr]
+  //     .sort(() => Math.random() - 0.5)
+  //     // .map gives each card object in array an id
+  //     .map((card) => ({ ...card, id: uniqid() }));
+  // };
 
   return (
     <>
@@ -53,17 +68,29 @@ const App = (props) => {
         <div className="logo">MEMORY GAME</div>
         <Scoreboard turns={turns} />
       </div>
-      <button className="new-game" onClick={shuffleCards} style={showOrHide}>
+      <button className="new-game" onClick={shuffleImgs} style={showOrHide}>
         <span class="material-symbols-outlined">play_circle</span>
       </button>
+      <div className="info">
+        <h2>How to Play</h2>
+        <ul>
+          <li>Click each image once</li>
+          <li>Pressing the same image twice resets the score</li>
+          <li>Win by scoring 12 points</li>
+        </ul>
+        <p>Good luck!</p>
+      </div>
       <div className="container">
-        {cards.map((card) => (
-          <div
-            className={"card " + card.src}
-            key={card.id}
-            onClick={shuffleCards}
-          ></div>
-        ))}
+        {renderOnPlay &&
+          giphy.map((img) => (
+            <img
+              className={`card ${img.src}`}
+              src={img.src}
+              alt="randomGiphyImg"
+              key={uniqid()}
+              onClick={shuffleImgs}
+            ></img>
+          ))}
       </div>
     </>
   );
